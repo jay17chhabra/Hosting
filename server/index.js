affb5381929469b1f290f5543ac18750df9594a1
Jay Chhabra <jaychhabra1704@gmail.com>
2024-08-13 19:10:44 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(bodyParser.json());
 
 const corsOptions = {
-  origin: ['https://hosting-client.vercel.app', 'https://hosting-client-git-main-jay-chhabras-projects.vercel.app', 'https://hosting-client-7hq8dztrv-jay-chhabras-projects.vercel.app'],
+  origin: '*', // Allow all origins temporarily for debugging
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization'],
